205a9be8f77bef1f09ee7b72d9a438874485df14
Kysaka <vlad.gorlach.2013@gmail.com>
2023-08-18 22:13:26 +0300

    Added Task

--- note_manager.py
+++ note_manager.py
@@ -1,87 +1,99 @@
+import csv
 import os
-import pickle
+from datetime import datetime
 
-class Note:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
+# Путь к файлу для хранения заметок
+FILE_PATH = "notes.csv"
 
-class NoteManager:
-    def __init__(self, filename="notes.pkl"):
-        self.filename = filename
-        self.notes = []
+def save_notes(notes):
+    with open(FILE_PATH, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=";")
+        writer.writerow(["ID", "Заголовок", "Тело", "Дата/Время"])
+        for note in notes:
+            writer.writerow(note)
 
-        if os.path.exists(filename):
-            with open(filename, "rb") as f:
-                self.notes = pickle.load(f)
+def load_notes():
+    if not os.path.exists(FILE_PATH):
+        return []
+    
+    with open(FILE_PATH, "r") as file:
+        reader = csv.reader(file, delimiter=";")
+        next(reader)  # Пропускаем заголовок
+        notes = [row for row in reader]
+    return notes
 
-    def save_notes(self):
-        with open(self.filename, "wb") as f:
-            pickle.dump(self.notes, f)
-
-    def create_note(self, title, content):
-        note = Note(title, content)
-        self.notes.append(note)
-        self.save_notes()
-
-    def read_notes(self):
-        for idx, note in enumerate(self.notes, start=1):
-            print(f"Note {idx} - {note.title}")
-
-    def view_note(self, note_index):
-        if 1 <= note_index <= len(self.notes):
-            note = self.notes[note_index - 1]
-            print(f"Title: {note.title}\nContent: {note.content}")
-
-    def edit_note(self, note_index, new_title, new_content):
-        if 1 <= note_index <= len(self.notes):
-            self.notes[note_index - 1].title = new_title
-            self.notes[note_index - 1].content = new_content
-            self.save_notes()
-
-    def delete_note(self, note_index):
-        if 1 <= note_index <= len(self.notes):
-            del self.notes[note_index - 1]
-            self.save_notes()
+def print_notes(notes):
+    for note in notes:
+        print(f"ID: {note[0]}")
+        print(f"Заголовок: {note[1]}")
+        print(f"Тело: {note[2]}")
+        print(f"Дата/Время: {note[3]}")
+        print("=" * 30)
 
 def main():
-    note_manager = NoteManager()
-
+    notes = load_notes()
+    
     while True:
-        print("\nNote Manager Menu:")
-        print("1. Create Note")
-        print("2. Read Notes")
-        print("3. View Note")
-        print("4. Edit Note")
-        print("5. Delete Note")
-        print("6. Exit")
-
-        choice = input("Select an option: ")
-
-        if choice == "1":
-            title = input("Enter note title: ")
-            content = input("Enter note content: ")
-            note_manager.create_note(title, content)
-        elif choice == "2":
-            note_manager.read_notes()
-        elif choice == "3":
-            note_manager.read_notes()
-            note_index = int(input("Enter note number to view: "))
-            note_manager.view_note(note_index)
-        elif choice == "4":
-            note_manager.read_notes()
-            note_index = int(input("Enter note number to edit: "))
-            new_title = input("Enter new title: ")
-            new_content = input("Enter new content: ")
-            note_manager.edit_note(note_index, new_title, new_content)
-        elif choice == "5":
-            note_manager.read_notes()
-            note_index = int(input("Enter note number to delete: "))
-            note_manager.delete_note(note_index)
-        elif choice == "6":
+        print("Введите команду:")
+        print("1 - Вывести список заметок")
+        print("2 - Показать заметку по ID")
+        print("3 - Добавить заметку")
+        print("4 - Редактировать заметку")
+        print("5 - Удалить заметку")
+        print("0 - Выход")
+        
+        command = input()
+        
+        if command == "1":
+            print_notes(notes)
+        elif command == "2":
+            note_id = input("Введите ID заметки: ")
+            for note in notes:
+                if note[0] == note_id:
+                    print("Найденная заметка:")
+                    print(f"ID: {note[0]}")
+                    print(f"Заголовок: {note[1]}")
+                    print(f"Тело: {note[2]}")
+                    print(f"Дата/Время: {note[3]}")
+                    break
+            else:
+                print("Заметка не найдена.")
+        elif command == "3":
+            title = input("Введите заголовок заметки: ")
+            body = input("Введите тело заметки: ")
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            new_note = [str(len(notes) + 1), title, body, timestamp]
+            notes.append(new_note)
+            save_notes(notes)
+            print("Заметка успешно добавлена.")
+        elif command == "4":
+            note_id = input("Введите ID заметки для редактирования: ")
+            for note in notes:
+                if note[0] == note_id:
+                    new_title = input("Введите новый заголовок заметки: ")
+                    new_body = input("Введите новое тело заметки: ")
+                    note[1] = new_title
+                    note[2] = new_body
+                    note[3] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    save_notes(notes)
+                    print("Заметка успешно отредактирована.")
+                    break
+            else:
+                print("Заметка не найдена.")
+        elif command == "5":
+            note_id = input("Введите ID заметки для удаления: ")
+            for note in notes:
+                if note[0] == note_id:
+                    notes.remove(note)
+                    save_notes(notes)
+                    print("Заметка успешно удалена.")
+                    break
+            else:
+                print("Заметка не найдена.")
+        elif command == "0":
             break
         else:
-            print("Invalid choice. Please select a valid option.")
+            print("Неверная команда. Попробуйте снова.")
 
 if __name__ == "__main__":
     main()
